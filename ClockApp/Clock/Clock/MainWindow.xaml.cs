589c7bb5b272ab2f936d775d9cf571ca86edaed2
mlkmhmtbyk <biyikm16@itu.edu.tr>
2019-11-12 07:32:12 +0300

    Sceduling Functions Added

--- ClockApp/Clock/Clock/MainWindow.xaml.cs
+++ ClockApp/Clock/Clock/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Clock
 {
@@ -24,5 +25,86 @@ namespace Clock
         {
             InitializeComponent();
         }
+
+
+
+        public static void WriteToTxt(string work, string day)
+        {
+            try
+            {
+                
+                StreamWriter file = File.AppendText("scedule.txt");
+                file.WriteLine(day + ": " + work);
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("This writing to text failed:", ex);
+            }
+        }
+
+        private void SundayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Sunday";
+            WriteToTxt(work, day);
+        }
+
+        private void MondayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Monday";
+            WriteToTxt(work, day);
+        }
+
+        private void TuesdayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Tuesday";
+            WriteToTxt(work, day);
+        }
+
+        private void WednesdayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Wednesday";
+            WriteToTxt(work, day);
+        }
+        private void ThursdayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Thursday";
+            WriteToTxt(work, day);
+
+        }
+        private void FridayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Friday";
+            WriteToTxt(work, day);
+        }
+        private void SaturdayClick(object sender, RoutedEventArgs e)
+        {
+            string work;
+            work = new TextRange(textbox.Document.ContentStart, textbox.Document.ContentEnd).Text;
+            string day;
+            day = "Saturday";
+            WriteToTxt(work, day);
+        }
+
+
+
     }
 }
